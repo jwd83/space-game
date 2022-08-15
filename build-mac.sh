67e5bd6027d3c26d9b99e090ac67d545ea3923be
Jared De Blander <jared@ayrscott.com>
2022-08-15 01:11:32 -0400

    mac build script

---
+++ build-mac.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Requires the package PyInstaller to be installed.
+
+# run the build script for macos
+python3 -m PyInstaller --onefile main.py
+
+# copy in the ships folder to the dist folder
+cp -rfv ships dist/
+
+# copy in the sounds folder to the dist folder
+cp -rfv sounds dist/
+
+
