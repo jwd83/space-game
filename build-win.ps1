f000ed555a1c857f562e34770a5cd5c100d21864
Jared De Blander <jared0x90@gmail.com>
2022-08-16 14:59:23 -0400

    windows build script in powershell

---
+++ build-win.ps1
@@ -0,0 +1,13 @@
+# remove the dist directory
+
+rm -r -force dist/
+
+PyInstaller --onefile main.py
+
+cp -r ships ./dist/
+cp -r sounds ./dist/
+
+rm -force windows.zip
+
+# compress the dist directory into the file windows.zip
+Compress-Archive dist/ windows.zip
