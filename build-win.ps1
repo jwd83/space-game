e6aef1bc2b1fdc6163567076374bd598205ffeb7
Jared De Blander <jared0x90@gmail.com>
2023-03-28 02:07:56 -0400

    updated build scripts a bit

--- build-win.ps1
+++ build-win.ps1
@@ -4,11 +4,12 @@ rm -r -force dist/
 
 PyInstaller --onefile main.py
 
+cp -r fonts ./dist/
 cp -r ships ./dist/
 cp -r sounds ./dist/
 cp -r sprites ./dist/
 
-rm -force windows.zip
+rm -force the-hunt-for-roy-carnassus-windows.zip
 
 # compress the dist directory into the file windows.zip
-Compress-Archive dist/ windows.zip
+Compress-Archive dist/ the-hunt-for-roy-carnassus-windows.zip
