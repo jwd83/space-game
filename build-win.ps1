353f75fc9a9777fcc2ff12af5c532dcb0d6279de
Jared De Blander <jared0x90@gmail.com>
2023-04-15 15:41:21 -0400

    cleaned up windows builder so extracted folder is the-hunt-for-roy-carnassus

--- build-win.ps1
+++ build-win.ps1
@@ -1,6 +1,6 @@
 # remove the dist directory
 
-rm -r -force dist/
+rm -r -force ./the-hunt-for-roy-carnassus/
 
 PyInstaller --onefile main.py
 
@@ -9,7 +9,12 @@ cp -r ships ./dist/
 cp -r sounds ./dist/
 cp -r sprites ./dist/
 
-rm -force the-hunt-for-roy-carnassus-windows.zip
+rm -force ./the-hunt-for-roy-carnassus-windows.zip
+
+mv ./dist/ ./the-hunt-for-roy-carnassus/
 
 # compress the dist directory into the file windows.zip
-Compress-Archive dist/ the-hunt-for-roy-carnassus-windows.zip
+Compress-Archive ./the-hunt-for-roy-carnassus/ ./the-hunt-for-roy-carnassus-windows.zip
+
+# clean up the dist folder
+rm -r -force ./the-hunt-for-roy-carnassus/
